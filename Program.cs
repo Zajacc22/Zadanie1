390ca19330bec7b3ef64471bd866feec45b0fde5
Zajacc22 <42608604+Zajacc22@users.noreply.github.com>
2021-04-20 23:29:46 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = "";
+            int i;
+            Console.WriteLine("Podaj liczbe:");
+            i = int.Parse(Console.ReadLine());
+            if (i % 3 == 0)
+            {
+                result += "fizz";
+            }
+            if (i % 5 == 0)
+            {
+                result += "buzz";
+            }
+            if (result == "")
+            {
+                Console.WriteLine(i);
+            }
+            else Console.WriteLine(result);
+        }
+    }
+}
